28e06426531646f4009b77e85ee5629eaf3d3bbc
Marcos Teodoro <marcosvinici_us@live.com>
2021-03-31 00:18:02 -0300

    Implementação do TOGGLE_TODO e mais testes para o ADD_TODO

--- src/redux-flow/reducers/todos/index.js
+++ src/redux-flow/reducers/todos/index.js
@@ -1,7 +1,24 @@
 'use strict'
 
-const todos = () => {
-  return [{ id: 0, text: 'Hey', completed: false }]
+import { ADD_TODO, TOGGLE_TODO } from './actions'
+
+const initialState = []
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat({
+        id: action.payload.id,
+        text: action.payload.text,
+        completed: false
+      })
+    case TOGGLE_TODO:
+      return state.map(todo => ({
+        ...todo,
+        completed: todo.id === action.payload.id ? !todo.completed : todo.completed
+      }))
+  }
+
 }
 
 export default todos
