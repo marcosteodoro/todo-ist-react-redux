6d8be8ca4158a8ee9c28f9c4d0af9b35bb83537f
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-01 16:13:08 -0300

    Criação do actions-creators

---
+++ src/redux-flow/reducers/todos/action-creators.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import { v4 } from 'uuid'
+
+import { ADD_TODO } from './actions'
+
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  payload: {
+    id: v4(),
+    text
+  }
+})
