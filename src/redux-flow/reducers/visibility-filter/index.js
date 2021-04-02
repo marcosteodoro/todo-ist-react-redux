b6731b0db68ca3512a371168a44a29ce873911a2
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-02 17:55:26 -0300

    Implementação do reducer de visibility filter

---
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions';
+
+export const initialState = SHOW_ALL
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.payload.filter
+  }
+
+  return state
+}
