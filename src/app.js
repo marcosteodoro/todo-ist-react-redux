9c00d92b4f036d6d5cb98825049e78efac8a66e8
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-04 18:15:27 -0300

    Refatoração da aplicação componentizando blocos do app

--- src/app.js
+++ src/app.js
@@ -1,52 +1,17 @@
 'use strict'
 
 import React from 'react'
-import { connect } from 'react-redux'
 
-import { addTodo, toggleTodo } from 'reducers/todos/action-creators'
+import Filter from 'components/filter'
+import Form from 'components/form'
+import TodosList from 'components/todos-list'
 
-const App = ({ todos, handleAddTodo, handleToggleTodo }) => (
+const App = () => (
   <div>
-    <form onSubmit={handleAddTodo}>
-      <input type='text' name='todo' />
-      <button type='submit'>Adicionar</button>
-    </form>
-    <ul>
-      {todos.map(todo => (
-        <li
-          key={todo.id}
-          style={{
-            textDecoration: todo.completed ? 'line-through' : 'none',
-            cursor: 'pointer'
-          }}
-          onClick={handleToggleTodo(todo.id)}
-        >
-          {todo.text}
-        </li>
-      ))}
-    </ul>
-
-    <div>
-      <h3>Mostrar</h3>
-      <span>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
-    </div>
+    <Form />
+    <TodosList />
+    <Filter />
   </div>
 )
 
-const mapStateToProps = (state) => ({
-  todos: state.todos
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  handleAddTodo: (e) => {
-    e.preventDefault()
-    dispatch(addTodo(e.target.todo.value))
-    e.target.todo.value = ''
-  },
-
-  handleToggleTodo: id => (e) => {
-    dispatch(toggleTodo(id))
-  }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
