bf57ec0becc624a92cadb8f357ce709fba886ce3
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-07 00:34:05 -0300

    Implementação do create reducer padrão para a aplicação

--- src/redux-flow/reducers/todos/index.js
+++ src/redux-flow/reducers/todos/index.js
@@ -1,25 +1,18 @@
 'use strict'
 
 import { ADD_TODO, TOGGLE_TODO } from './actions'
+import createReducer from '../create-reducer'
 
 export const initialState = []
 
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: action.payload.id,
-        text: action.payload.text,
-        completed: false
-      })
-    case TOGGLE_TODO:
-      return state.map(todo => ({
-        ...todo,
-        completed: todo.id === action.payload.id ? !todo.completed : todo.completed
-      }))
-  }
-
-  return state
-}
-
-export default todos
+export default createReducer(initialState, {
+  [ADD_TODO]: (state, action) => state.concat({
+    id: action.payload.id,
+    text: action.payload.text,
+    completed: false
+  }),
+  [TOGGLE_TODO]: (state, action) => state.map(todo => ({
+    ...todo,
+    completed: todo.id === action.payload.id ? !todo.completed : todo.completed
+  }))
+})
--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -1,14 +1,10 @@
 'use strict'
 
 import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions'
+import createReducer from '../create-reducer'
 
 export const initialState = SHOW_ALL
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.payload.filter
-  }
-
-  return state
-}
+export default createReducer(initialState, {
+  [SET_VISIBILITY_FILTER]: (state, action) => action.payload.filter
+})
