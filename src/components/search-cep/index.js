33f110d3805bf1f0e99e97525190d1c11cdc25a2
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-08 17:48:41 -0300

    Melhoria nas mensagens de erro de CEP não encontrado

--- src/components/search-cep/index.js
+++ src/components/search-cep/index.js
@@ -11,19 +11,21 @@ class SearchCepContainer extends PureComponent {
     code: '',
     district: '',
     state: '',
-    status: 200
+    status: 200,
+    isFetching: false
   }
 
   handleSubmit = async (e) => {
     e.preventDefault()
-
+    this.setState({ isFetching: true })
     const cep = e.target.cep.value
 
     const response = await ajax().get('https://apps.widenet.com.br/busca-cep/api/cep.json', { code: cep })
+    this.setState({ isFetching: false })
     this.setState(response)
   }
 
-  render () {
+  render() {
     return (
       <SearchCep
         {...this.state}
